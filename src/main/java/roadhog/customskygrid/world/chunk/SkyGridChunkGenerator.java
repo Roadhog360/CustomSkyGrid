7526163da5b0512784dad4ebe6d3691da9ca9069
Roadhog360 <Roadhog360@outlook.com>
2024-07-20 02:16:43 -0500

    Basic grid generation, decorator hasn't been adjusted yet.

--- src/main/java/roadhog/customskygrid/world/chunk/SkyGridChunkGenerator.java
+++ src/main/java/roadhog/customskygrid/world/chunk/SkyGridChunkGenerator.java
@@ -15,6 +15,7 @@ import net.minecraft.util.math.random.CheckedRandom;
 import net.minecraft.util.math.random.ChunkRandom;
 import net.minecraft.util.math.random.RandomSeed;
 import net.minecraft.world.ChunkRegion;
+import net.minecraft.world.HeightLimitView;
 import net.minecraft.world.Heightmap;
 import net.minecraft.world.biome.GenerationSettings;
 import net.minecraft.world.biome.source.BiomeAccess;
@@ -29,33 +30,53 @@ import net.minecraft.world.gen.carver.CarverContext;
 import net.minecraft.world.gen.carver.CarvingMask;
 import net.minecraft.world.gen.carver.ConfiguredCarver;
 import net.minecraft.world.gen.chunk.*;
+import net.minecraft.world.gen.densityfunction.DensityFunctionTypes;
 import net.minecraft.world.gen.noise.NoiseConfig;
+import org.apache.commons.lang3.mutable.MutableObject;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Iterator;
 import java.util.Objects;
+import java.util.OptionalInt;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
+import java.util.function.Predicate;
 
 public class SkyGridChunkGenerator extends NoiseChunkGenerator {
-    public static final Codec<SkyGridChunkGenerator> CODEC = RecordCodecBuilder.create((instance) -> {
-        return instance.group(BiomeSource.CODEC.fieldOf("biome_source").forGetter((generator) -> {
-            return generator.biomeSource;
-        }), ChunkGeneratorSettings.REGISTRY_CODEC.fieldOf("settings").forGetter((generator) -> {
-            return generator.settings;
-        })).apply(instance, instance.stable(SkyGridChunkGenerator::new));
-    });
+    public static final Codec<SkyGridChunkGenerator> CODEC = RecordCodecBuilder.create(
+            (instance) -> instance.group(BiomeSource.CODEC.fieldOf("biome_source")
+                    .forGetter((generator) -> generator.biomeSource), ChunkGeneratorSettings.REGISTRY_CODEC.fieldOf("settings")
+                    .forGetter((generator) -> generator.settings)).apply(instance, instance.stable(SkyGridChunkGenerator::new)));
 
     public SkyGridChunkGenerator(BiomeSource biomeSource, RegistryEntry<ChunkGeneratorSettings> settings) {
         super(biomeSource, settings);
+    }
+    public int getSpawnHeight(HeightLimitView world) {
+        return 64;
+    }
+
+    private OptionalInt sampleHeightmap(HeightLimitView world, NoiseConfig noiseConfig, int x, int z, @Nullable MutableObject<VerticalBlockSample> columnSample, @Nullable Predicate<BlockState> stopPredicate) {
+        return OptionalInt.of(getSpawnHeight(world));
     }
 
     public void carve(ChunkRegion chunkRegion, long seed, NoiseConfig noiseConfig, BiomeAccess biomeAccess, StructureAccessor structureAccessor, Chunk chunk, GenerationStep.Carver carverStep) {
     }
 
     public CompletableFuture<Chunk> populateNoise(Executor executor, Blender blender, NoiseConfig noiseConfig, StructureAccessor structureAccessor, Chunk chunk) {
-        chunk.setBlockState(new BlockPos(chunk.getPos().x * 16, 64, chunk.getPos().z * 16),
-                Blocks.GRASS_BLOCK.getDefaultState(), false);
+        for (int x = 0; x < 4; x++) {
+            for (int z = 0; z < 4; z++) {
+                for (int y = -16; y <= 16; y++) {
+                    BlockPos pos = new BlockPos(
+                            chunk.getPos().getStartPos().getX() + (x * 4),
+                            y * 4,
+                            chunk.getPos().getStartPos().getZ() + (z * 4));
+                    chunk.setBlockState(pos,
+                            y == 16 ? Blocks.GRASS_BLOCK.getDefaultState() : Blocks.STONE.getDefaultState(),
+                            false);
+                }
+            }
+        }
         return CompletableFuture.completedFuture(chunk);
     }
 }
